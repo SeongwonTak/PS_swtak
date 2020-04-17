7418595ae57259fe2c7a463b0de8ced497edf5e5
SeongwonTak <mathlove44@naver.com>
2020-04-17 21:53:31 +0900

    # bfs_토마토 재 풀이... # 하지만 그래도 TLE

--- BOJ_7576.py
+++ BOJ_7576.py
@@ -4,7 +4,7 @@
 def solve(tomatoes, n, m):
     way_x = [-1, 1, 0, 0]  # 좌 / 우 / 상 / 하 x값 변화
     way_y = [0, 0, -1, 1]  # 좌 / 우 / 상 / 하 y값 변화
-    visited = [[False] * m for _ in range(n)]
+    visited = [[0] * m for _ in range(n)]
     next_block = []
     end_judge = 1
     # 첫 시작점들은 1인 좌표들에 대해서 넣어야 한다.
@@ -19,30 +19,25 @@ def solve(tomatoes, n, m):
         return 0
 
     else:  # 이제 돌아줘야 함
-        day_val = 0
+        day_val=0
+        visited[0][0] = 0
         while next_block:  # 더 꺼낼게 없을 때 까지
             x, y = next_block.pop(0)  # 꺼낼 거의 첫번째 위치
-            if not visited[x][y]:
-                visited[x][y] = True
-                day_val = tomatoes[x][y]  # 지금 까지 걸린일자
 
-                for i in range(4):
-                    # 일단 4 방향중 하나를 들어가자
-                    new_x = x + way_x[i]
-                    new_y = y + way_y[i]
-                    # 영역이 넘어갔으면 Fail
-                    if new_x < 0 or new_y < 0 or new_x >= n or new_y >= m:
-                        continue
+            for i in range(4):
+                # 일단 4 방향중 하나를 들어가자
+                new_x = x + way_x[i]
+                new_y = y + way_y[i]
+                # 영역이 넘어갔으면 Fail
+                if new_x < 0 or new_y < 0 or new_x >= n or new_y >= m:
+                    continue
 
-                    if not visited[new_x][new_y]:
-                        new_day_val = tomatoes[new_x][new_y]
-
-                        if new_day_val == -1:  # 벽은 돌지 않아요
-                            continue
-
-                        elif new_day_val == 0 or day_val + 1 < new_day_val:  # 역류방지구문
-                            tomatoes[new_x][new_y] = day_val + 1  # 이게 실행된다는 건, 새로운 길이다.
-                            next_block.append([new_x, new_y])  # val + 1이 같거나 크다는건 이미 체크한 칸
+                else:
+                    if visited[new_x][new_y] == 0 and tomatoes[new_x][new_y] == 0:
+                        tomatoes[new_x][new_y] = 1
+                        visited[new_x][new_y] = visited[x][y] + 1
+                        day_val = visited[new_x][new_y]
+                        next_block.append((new_x, new_y))
 
         # 안 된 칸이 있나 체크
         end_judge_t = 1
@@ -56,7 +51,7 @@ def solve(tomatoes, n, m):
             return -1
 
         else:
-            return (day_val-1)
+            return (day_val)
 
 
 def main():
