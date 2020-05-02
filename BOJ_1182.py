e030edf6573666d29e0fa07c7c581315a7d5eef7
SeongwonTak <mathlove44@naver.com>
2020-05-02 18:36:37 +0900

    #개고생해서 푼 DFS 문제  #순열인지 조합인지 잘 읽자.

--- BOJ_1182.py
+++ BOJ_1182.py
@@ -1,15 +1,36 @@
 # 부분 수열의 합
 # https://www.acmicpc.net/problem/1182
-def dfs(depth, num, sum):
-    pass
+def sum_check(result, sum):
+    check_sum = 0
+    for i in range(0, len(result)):
+        check_sum += result[i]
+    if check_sum == sum:
+        return True
+    else:
+        return False
 
-def main():
-    num, sum = map(int, input().split())
-    num_list = list(map(int, input().split()))
-    num_list.append(0) #이게 바로 합이 맞을때마다 1이 올라갈 변수다. 맨 마지막 칸이다.
+num, sum = map(int, input().split())
+num_list = list(map(int, input().split()))
+visited = [0] * num
+count = 0
+result = []
+def dfs(depth, L, num, sum):
+    global count
 
-    dfs(0, num, sum)
+    if depth == num:
+        return
 
+    for i in range(L, num):
+        visited[i] = True
+        L = i+1
+        result.append(num_list[i])
 
-if __name__ == "__main__":
-    main()
+        if sum_check(result, sum):
+            count += 1
+
+        dfs(depth+1, L, num, sum)
+        visited[i] = False
+        result.pop()
+
+dfs(0, 0, num, sum)
+print(count)
